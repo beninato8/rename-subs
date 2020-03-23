31dc27677dc3a93d63d8f75bc43cf1def7a01f41
Nicholas Beninato <beninato8@gmail.com>
2020-03-23 04:00:41 -0700

    cleaner output for selection

--- subscene.py
+++ subscene.py
@@ -12,13 +12,13 @@ from selenium.webdriver.common.keys import Keys
 import re
 import time
 import sys
+
 url = 'https://subscene.com/'
 
 testing_mode = False
 
 if not testing_mode:
     args = sys.argv
-    # args += ['Godzilla']
     if len(args) < 2:
         print('Please enter a movie name')
         exit()
@@ -41,8 +41,6 @@ if not testing_mode:
     options.add_argument('chromever="73.0.3683.68"')
 
     browser = webdriver.Chrome(chromedriver, options=options)
-
-
     browser.get(url)
 
     login = browser.find_element_by_id('query')
@@ -52,19 +50,31 @@ if not testing_mode:
     soup = bs4(browser.page_source, 'html.parser')
     browser.quit()
 
-    for i, x in enumerate(soup.find_all('div', {'class':'title'})):
+    idx = 0
+    for x in soup.find_all('div', {'class':'title'}):
         for a in x.find_all('a'):
-            d[i] = {'url':f"{url}{a['href']}", 'name':a.contents[0]}
-            # print(i, f"{url}{a['href']}", a.contents[0])
+            if len(d) == 0:
+                d[idx] = {'url':f"{url}{a['href']}", 'name':a.contents[0]}
+                idx += 1
+            elif not any(v['name'] == a.contents[0] for v in d.values()):
+                d[idx] = {'url':f"{url}{a['href']}", 'name':a.contents[0]}
+                idx += 1
+                # print(i, f"{url}{a['href']}", a.contents[0])
 
     print()
-    print('\n'.join(f'{k}: {v["name"]}' for k,v in d.items()))
+    print('\n'.join(f'{str(k).rjust(len(str(idx)), " ")}: {v["name"]}' for k,v in d.items()))
     print('Chose a number')
-    user_in = int(input())
+    try:
+        user_in = int(input())
+    except Exception as e:
+        user_in = -1
     while user_in not in d.keys():
         print('Please enter a valid number')
-        print('\n'.join(f'{k}: {v["name"]}' for k,v in d.items()))
-        user_in = int(input())
+        print('\n'.join(f'{str(k).rjust(len(str(idx)), " ")}: {v["name"]}' for k,v in d.items()))
+        try:
+            user_in = int(input())
+        except Exception as e:
+            user_in = -1
     # pprint(d)
     # time.sleep(3)
 
@@ -81,21 +91,27 @@ for x in soup.find_all('a', {'href':True}):
         if 'English' == y.contents[0].strip():
             l.append(x)
 d = {}
-for i,x in enumerate(l):
+idx = 0
+for x in l:
     # print(x.previous_element.previous_element.previous_element.previous_element.prettify())
     # print('\n\n\n\n\n')
 
     parent = x.previous_element.previous_element.previous_element.previous_element
     # print(parent.prettify())
-    file = parent.find_all('span')[1].contents[0].rstrip().strip()
+    file = parent.find_all('span')[1].contents[0].rstrip().strip().replace('/', '')
     try:
         author = parent.find_all('td', {'class':'a5'})[0].find_all('a')[0].contents[0].rstrip().strip()
     except Exception as e:
         author = 'Anonymous'
     desc = parent.find_all('div')[0].contents[0].rstrip().strip().replace('\n', ' ')
     link = url+parent.find('a')['href']
-    # print(link)
-    d[i] = {'file':file, 'author':author, 'desc': desc, 'url':link}
+    # print(link, any(link == v['url'] for v in d.values()))
+    if len(d) == 0:
+        d[idx] = {'file':file[:50], 'author':author[:20], 'desc': '\n\t' + desc[:80], 'url':link}
+        idx += 1
+    elif not any(link == v['url'] for v in d.values()):
+        d[idx] = {'file':file[:50], 'author':author[:20], 'desc': '\n\t' + desc[:80], 'url':link}
+        idx += 1
 # exit()
 files = [len(x['file']) for x in d.values()]
 authors = [len(x['author']) for x in d.values()]
@@ -103,21 +119,23 @@ descs = [len(x['desc']) for x in d.values()]
 
 maxf = max(files) + 4
 maxa = max(authors) + 4
+maxa = 0
 maxd = max(descs) + 4
+maxd = 0
 
 def format_line(v):
     p1 = f"{v['file']}{' '*(maxf - len(v['file']))}"
     p2 = f"{v['author']}{' '*(maxa - len(v['author']))}"
     p3 = f"{v['desc']}{' '*(maxd - len(v['desc']))}"
-    return p1+p2+p3
+    return p1+p2+p3+('\n' * (len(v['desc'].strip()) != 0))
 
 print()
-print('\n'.join(f"{k}: {format_line(v)}" for k,v in d.items()))
+print('\n'.join(f"{str(k).rjust(len(str(idx)), ' ')}: {format_line(v)}" for k,v in d.items()))
 print('Chose a number')
 user_in = int(input())
 while user_in not in d.keys():
     print('Please enter a valid number')
-    print('\n'.join(f"{k}: {format_line(v)}" for k,v in d.items()))
+    print('\n'.join(f"{str(k).rjust(len(str(idx)), ' ')}: {format_line(v)}" for k,v in d.items()))
     user_in = int(input())
 
 html = requests.get(d[user_in]['url']).text
