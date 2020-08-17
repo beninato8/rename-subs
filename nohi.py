0c1e72543ddd12f6ec52f0c2a1114005dd990c98
Nicholas Beninato <beninato8@gmail.com>
2020-08-17 01:59:34 -0700

    only modify text, ignore timestamps

--- nohi.py
+++ nohi.py
@@ -2,59 +2,86 @@
 
 import sys
 import re
+from pprint import pprint
 
 args = sys.argv
+# args += ['input.srt']
 
 if len(args) != 2:
     print('Only enter one file')
     exit()
 
+print(args[1])
+
 try:
     with open(args[1], 'r') as f:
-        content = f.read()
+        input_srt = f.read()
 except Exception as e:
     with open(args[1], 'r', encoding='latin-1') as f:
-        content = f.read()
+        input_srt = f.read()
 
+class Section():
+    def __init__(self, *lines):
+        try:
+            self.num = str(lines[0])
+            self.time = str(lines[1])
+            self.text = '\n'.join(lines[2:])
+        except Exception as e:
+            print(lines)
+            raise e
 
-blacklist = {'Sk0r', 'yomoy', 'Collioure', 'Feygnasse Team', 'La Fabrique', 'mpm', 'Jarick', 'do_Ob', 'The Ni.Knight', r'mkvcage\.com', 'Encoded by Hunter', 'Crazy4TV.com'}
+    def clean(self):
+        debug = False
+        if self.num == '12300':
+            debug = True
+        content = self.text
 
-content = re.sub(r'pos\(.*\)', r'', content)
+        blacklist = {}
+        content = re.sub(r'{\\an\d+}', r'', content)
+        content = re.sub(r'pos\(.*\)', r'', content)
+        content = re.sub(r'([\[\(].*\n?.*[\]\)] ?)|([^\d\n]+: ?)|([{}]+)', r'', content)
+        content = re.sub(r'{.+}', r'', content)
+        content = re.sub(r'{?[aA][nN]\d+}?', r'', content)
+        content = re.sub(r'[ ]{2,}', r' ', content)
+        content = re.sub(r'.*[\[\]].*', r'', content)
+        content = re.sub(r'{|}', r'', content)
+        content = re.sub(r'<[^>]*>', r'', content)
+        content = re.sub(r"'’", r"'", content)
+        content = re.sub(r"’", r"'", content)
+        content = re.sub(r'"', r"'", content)
+        content = re.sub(r"\*", r"", content)
+        content = re.sub(r"&quot;", r'"', content)
+        content = re.sub(r".*: *", r'', content)
+        content = re.sub(r"(?<=[^\w])i(?=[^\w])", r'I', content)
+        content = re.sub(r'\n[-](?=\n)', r'', content)
+        content = re.sub(r'^-\n', r'', content) # hypen at start of line
+        content = re.sub(r'-[\s]*$', r'', content) # hyphen at end of line
+        if debug:
+            print(content)
 
-content = re.sub(r'([\[\(].*\n?.*[\]\)] ?)|([^\d\n]+: ?)|([{}]+)', r'', content)
-content = re.sub(r'{.+}', r'', content)
-content = re.sub(r'{?[aA][nN]\d+}?', r'', content)
-content = re.sub(r'[ ]{2,}', r' ', content)
-content = re.sub(r'.*[\[\]].*', r'', content)
-content = re.sub(r'{|}', r'', content)
-content = re.sub(r'<[^>]*>', r'', content)
-content = re.sub(r"'’", r"'", content)
-content = re.sub(r"’", r"'", content)
-content = re.sub(r'"', r"'", content)
-content = re.sub(r"(?<=[^\d]):(?=[^\d])", r"", content)
-content = re.sub(r"\*", r"", content)
-content = re.sub(r"&quot;", r'"', content)
-content = re.sub(r"([^\d]+):\n?", r'', content)
-content = re.sub(r"(?<=[^\w])i(?=[^\w])", r'I', content)
+        if any(x in content for x in blacklist):
+            rgx = '|'.join(blacklist)
+            content = re.sub(rgx, '', content)
 
-# title
-words = ['jedi', 'lothal', 'hera', 'ezra', 'imperial', 'wookies', 'vizago', 'r2', 'd2', 'zeb', 'kanan', 'lasan', 'lasat', 'senator organa', 'phantom', 'sabine']
-cap = {x:x.title() for x in words}
-for k,v in cap.items():
-    content = re.sub(fr'(?<=[^\w]){k}(?=[^\w])', v, content)
+        content = content.strip().rstrip()
 
-# upper
-words = ['c-3po', 'c3-po', 'c3po']
-cap = {x:x.upper() for x in words}
-for k,v in cap.items():
-    content = re.sub(fr'(?<=[^\w]){k}(?=[^\w])', v, content)
+        content = re.sub(r'\n+', r'\n', content)
 
-if any(x in content for x in blacklist):
-    rgx = '|'.join(blacklist)
-    content = re.sub(rgx, '', content)
+        self.text = content
 
-# content = re.sub(r'\n[ -]+', r'\n', content)
+    def __str__(self):
+        return '\n'.join([self.num, self.time, self.text])
 
-# print(content)
+cleaned = []
+for x in input_srt.split('\n\n'):
+    if x != '':
+        sec = Section(*x.split('\n'))
+        sec.clean()
+        cleaned.append(str(sec))
+
+# print('\n\n'.join(cleaned))
+
+# exit()
+
 with open(args[1], 'w') as f:
-    f.write(content)
+    f.write('\n\n'.join(cleaned))
